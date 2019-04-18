2b8cb64696395acd18b083e36cc1ba7ce55d7eb9
leandrovianna <leandro97vianna@gmail.com>
2019-04-18 12:13:17 -0300

    small improvement in skeleton

--- Lexer.dart
+++ Lexer.dart
@@ -1,13 +1,55 @@
+import 'dart:io';
+import 'dart:collection';
+import 'dart:convert';
+
 class Token {
   String lexeme, token, type;
+
+  Token({this.lexeme, this.token, this.type});
+}
+
+class Pair<F, S> {
+  F first;
+  S second;
+
+  Pair(this.first, this.second);
 }
 
 class Lexer {
-  String filename;
+  String sourceCode;
+  int position;
 
-  Lexer(this.filename);
+  int state;
+  Map<Pair<int, String>, int> transitions;
+
+  Lexer(String sourceCodeFilename) {
+    var file = new File(sourceCodeFilename);
+    this.sourceCode = file.readAsStringSync();
+    this.position = 0;
+    this._constructTransitionTable();
+  }
 
   Token getToken() {
-    return null;
+    String buffer = "";
+
+    while (this.position < this.sourceCode.length) {
+      var pair = Pair<int, String>(this.state, this.sourceCode[this.position]);
+      if (this.transitions.containsKey(pair)) {
+        buffer += this.sourceCode[this.position];
+        this.state = this.transitions[pair];
+      } else {
+        break;
+      }
+
+      this.position++;
+    }
+
+    // TODO: Verify if this.state is final when no transitions is allowed or input is finished
+    return new Token(lexeme: buffer, token: buffer, type: '-');
+  }
+
+  void _constructTransitionTable() {
+    this.transitions = new HashMap();
+    // TODO: Construct transitions table of DFA
   }
 }
--- main.dart
+++ main.dart
@@ -1,6 +1,14 @@
 import 'Lexer.dart';
 
 main() {
-  var lexer = new Lexer('code.mgol');
-  lexer.getToken();
+  // cria um analisador lexico para o arquiv fonte texto.alg
+  var lexer = new Lexer('texto.alg');
+
+  // leitura dos tokens
+
+  var token = lexer.getToken();
+  while (token != null) {
+    print('${token.lexeme} ${token.token} ${token.type}');
+    token = lexer.getToken();
+  }
 }
